7c203d3f0fc113e47989c42936831eb0925ec2f2
vinay2006git <vinayyeltiwar@gmail.com>
2025-04-27 11:26:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useParams } 
 import axios from 'axios';
 import './App.css';
 
-const API = 'http://localhost:5000';
+const API = 'https://student-mangement-t77g.onrender.com';
 
 // Navbar Component
 const Navbar = () => {
